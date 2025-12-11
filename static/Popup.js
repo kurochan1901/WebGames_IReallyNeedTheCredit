8781b5575a950d44886ef05dada264a48705657b
kurochan1901 <superstickman19.mc@gmail.com>
2025-12-11 14:21:45 +0800

    Update userinfo --untested

---
+++ static/Popup.js
@@ -0,0 +1,11 @@
+// 顯示或隱藏彈出視窗
+function togglePopup() {
+    let popup = document.getElementById("user-popup");
+    popup.style.display = (popup.style.display === "block") ? "none" : "block";
+}
+
+// 關閉彈出視窗
+function closePopup() {
+    let popup = document.getElementById("user-popup");
+    popup.style.display = "none";
+}
